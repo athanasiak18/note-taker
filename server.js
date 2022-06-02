0b7e50c0bcc907da1d81267b2ce9ae93d6e16406
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 00:01:01 -0400

    parse

--- server.js
+++ server.js
@@ -5,4 +5,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const apiRoutes = require('./Develop/routes/apiRoutes');
+
 const htmlRoutes = require('./Develop/routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('./Develop/public'));
