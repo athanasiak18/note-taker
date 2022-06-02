590cbcf9dfd2aae9c73c33bbfbb4ba096e0f8481
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-01 23:53:41 -0400

    note routes

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -3,3 +3,6 @@ const { findById, createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 const uuid = require('../../helpers/uuid.js');
 
+router.get('/notes', (req, res) => {
+    res.json(notes)
+});
