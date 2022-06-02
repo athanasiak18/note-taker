18e0bdec8c783b35dd0655f830b2aaef6d8aceba
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 00:01:38 -0400

    listen

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('./Develop/public'));
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
