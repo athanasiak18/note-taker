f9810341ae797398f451638327f096e8467b9c7b
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-01 23:59:55 -0400

    server routes

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+const apiRoutes = require('./Develop/routes/apiRoutes');
+const htmlRoutes = require('./Develop/routes/htmlRoutes');
